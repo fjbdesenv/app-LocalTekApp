cd57d5797c3125de8843bbe591608b1f9578e8b2
Fábio Júnior Barbosa <fjb.desenv@gmail.com>
2024-02-01 06:17:49 -0300

    Adicionando form login

---
+++ src/components/Forms/FormLogin.vue
@@ -0,0 +1,50 @@
+<template>
+    <b-form class="bg-info-subtle m-3 border rounded-3 p-5" @submit="onSubmit">
+
+        <h1 class="text-center">Login</h1>
+
+        <b-form-group id="input-group-1" label="Email" label-for="email">
+            <b-form-input id="email" v-model="form.email" type="email" placeholder="Email" required
+                size="lg"></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-2" label="Senha" label-for="senha">
+            <b-form-input id="senha" v-model="form.senha" type="password" placeholder="Senha" required size="lg">
+            </b-form-input>
+        </b-form-group>
+
+        <b-container class="d-flex justify-content-center  mt-3 ">
+            <b-button squared type="submit" variant="primary" size="lg"> Logar </b-button>
+        </b-container>
+
+    </b-form>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { BContainer, BForm, BFormGroup, BFormInput, BButton } from 'bootstrap-vue-next';
+
+export default defineComponent({
+    name: 'FormLogin',
+    data: () => ({
+        form: {
+            email: '',
+            senha: ''
+        }
+    }),
+    components: {
+        BContainer,
+        BForm,
+        BFormGroup,
+        BFormInput,
+        BButton
+    },
+    methods: {
+        onSubmit(event: Event) {
+            event.preventDefault();
+            this.$emit('login', this.form);
+        }
+    }
+});
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,41 @@
+<template>
+  <b-container fluid class="d-flex align-items-center justify-content-center h-100 ">
+    <b-row>
+      <b-col cols="12">
+        <b-container class="w-100">
+          <FormLogin @login="login"/>
+        </b-container>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { BContainer, BRow, BCol } from 'bootstrap-vue-next';
+import FormLogin from '@/components/Forms/FormLogin.vue';
+import { Login } from '@/interfaces/Login'
+
+export default defineComponent({
+  name: 'LoginView',
+  data: () => ({
+    form: {
+      email: '',
+      senha: ''
+    },
+    loggingIn: false
+  }),
+  components: {
+    FormLogin,
+    BContainer,
+    BRow,
+    BCol,
+  },
+  methods:{
+    login(form: Login){
+      console.log(form);
+      this.loggingIn = true;
+    }
+  }
+});
+</script>
