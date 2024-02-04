9598aa19d53ca4b328fdc6049683c0993c72f560
Fábio Júnior Barbosa <fjb.desenv@gmail.com>
2024-02-04 12:16:59 -0300

    Adicionando menu remessa

--- src/views/modules/RemessasView.vue
+++ src/views/modules/RemessasView.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>Remessas</div>
+  <h1 class="text-center">Remessas</h1>
+  <MenuRemessa />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import MenuRemessa from "@/components/Menus/MenuRemessa.vue";
 
 export default defineComponent({
   name: "RemessasView",
+  components: {
+    MenuRemessa,
+  },
 });
 </script>
