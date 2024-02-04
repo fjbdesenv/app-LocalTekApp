10dc380edaf0e6e58823518c9b9729aba9a8292a
Fábio Júnior Barbosa <fjb.desenv@gmail.com>
2024-02-04 15:17:18 -0300

    Ajuste de layout de componets view de modulos

--- src/components/Menus/MenuRemessa.vue
+++ src/components/Menus/MenuRemessa.vue
@@ -1,18 +1,16 @@
 <template>
-  <div >
-    <b-nav vertical class="w-25 py-2 bg-black rounded-end-3">
-      <b-nav-item>
-        <b-button @click="logout()" variant="danger" class="btn-item">
-          <BIconPower /> Sair
-        </b-button>
-      </b-nav-item>
-      <b-nav-item v-for="(item, index) in menuRemessaItens" :key="index">
-        <router-link :to="{ name: item.link }">
-          <b-button class="btn-item"> {{ item.title }} </b-button>
-        </router-link>
-      </b-nav-item>
-    </b-nav>
-  </div>
+  <b-nav vertical class="py-2 menu">
+    <b-nav-item>
+      <b-button @click="logout()" variant="danger" class="btn-item logout">
+        <BIconPower /> Sair
+      </b-button>
+    </b-nav-item>
+    <b-nav-item v-for="(item, index) in menuRemessaItens" :key="index">
+      <router-link :to="{ name: item.link }">
+        <b-button class="btn-item"> {{ item.title }} </b-button>
+      </router-link>
+    </b-nav-item>
+  </b-nav>
 </template>
 
 <script lang="ts">
@@ -38,17 +36,24 @@ export default defineComponent({
 </script>
 
 <style scoped>
+.menu {
+  background-color: gray;
+}
 .btn-item {
   width: 100%;
+  background-color: rgba(0, 0, 0, 0.75);
 }
 .btn-item:hover {
   color: red;
   background-color: white;
-  box-shadow: 1px 1px 1px 1px rgba(255, 255, 255, 0.75);
 }
 
 .btn-item,
 .btn-item:hover {
   transition: all 1s;
+}
+
+.logout {
+  background-color: rgba(255, 0, 0, 0.75);
 }
 </style>
