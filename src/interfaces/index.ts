4ae6e3dc2994578176ccfa4d46ab0d99084ab3e6
Fábio Júnior Barbosa <fjb.desenv@gmail.com>
2024-02-08 07:15:50 -0300

    Refatoração de posicao de componentes

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -3,4 +3,9 @@ import { Module } from "./Module";
 import { ItemMenu } from "./ItemMenu";
 import { Status } from "./Status";
 
-export { Login, Module, ItemMenu, Status} 
+export {
+    Login,
+    Module,
+    ItemMenu,
+    Status
+} 
