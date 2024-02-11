1df6abd3c067e6b4e3036a715a43f39628f98fd9
Fábio Júnior Barbosa <fjb.desenv@gmail.com>
2024-02-11 14:11:52 -0300

    Adicionando CRUD Cliente

---
+++ src/components/Others/Cliente/ClienteCadastro.vue
@@ -0,0 +1,15 @@
+<template>
+  <FormCliente :cadastro="true" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import FormCliente from "@/components/Forms/FormCliente.vue";
+
+export default defineComponent({
+  name: "ClienteCadastro",
+  components: {
+    FormCliente,
+  },
+});
+</script>
---
+++ src/components/Others/Cliente/ClienteEditar.vue
@@ -0,0 +1,15 @@
+<template>
+  <FormCliente :cadastro="false" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import FormCliente from "@/components/Forms/FormCliente.vue";
+
+export default defineComponent({
+  name: "ClienteEditar",
+  components: {
+    FormCliente,
+  },
+});
+</script>
